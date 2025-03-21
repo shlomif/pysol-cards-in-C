c41f1b12fa7de36faf1675680875b92f057fa536
Shlomi Fish <shlomif@shlomifish.org>
2025-03-21 17:32:18 +0200

    WiP: py embed

--- multiply.py
+++ multiply.py
@@ -1,7 +1,15 @@
-def multiply(a,b):
+from pysol_cards.deal_game import Game
+
+
+def multiply(a, b):
     print("Will compute", a, "times", b)
     c = 0
     for i in range(0, a):
         c = c + b
     return c
 
+
+class Gen:
+    def __init__(self):
+        self.game_variant = 'black_hole'
+        self.game = Game(self.game_variant, 1, self.which_deals, 13)
