b9ca550187f944978aefa7107315759b55b52e93
Shlomi Fish <shlomif@shlomifish.org>
2025-03-21 17:20:26 +0200

    WiP: py embed

---
+++ Makefile
@@ -0,0 +1,12 @@
+#
+# Makefile
+# Shlomi Fish, 2025-03-21 17:00
+#
+
+all: call.exe
+
+call.exe: call.c
+	gcc ` python3-config --cflags ` -o $@ $< ` python3-config --ldflags --libs ` -l python3.11
+
+# vim:ft=make
+#
---
+++ run.bash
@@ -0,0 +1,12 @@
+#! /usr/bin/env bash
+#
+# run.bash
+# Copyright (C) 2025 Shlomi Fish < https://www.shlomifish.org/ >
+#
+# Distributed under the terms of the MIT license.
+#
+
+
+
+export PYTHONPATH="`pwd`"
+./call.exe multiply multiply 3 8
