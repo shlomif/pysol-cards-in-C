09e438a1720f2c6054aebea262e06590ba161e6c
Shlomi Fish <shlomif@shlomifish.org>
2025-04-01 12:19:08 +0300

    pysolcards.py-for-C progress

--- multiply.py
+++ multiply.py
@@ -1,5 +1,9 @@
 # from pysol_cards.deal_game import Game
 
+# from pysol_cards.cards import CardRenderer
+from pysol_cards.deal_game import Game
+from pysol_cards.random import RandomBase
+
 
 def pythonish_multiply(a, b):
     print("Will compute", a, "times", b)
@@ -9,7 +13,12 @@ def pythonish_multiply(a, b):
     return c
 
 
-from pysol_cards.deal_game import Game
+game_variant = "freecell"
+ms = True
+which_deals = (RandomBase.DEALS_MS if ms else RandomBase.DEALS_PYSOLFC)
+game = Game(game_variant, 1, which_deals, 13)
+
+
 def gen_pysol_deal(game_variant, deal_idx):
     return "[]"
 
