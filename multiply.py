aec7729f874dc0347e2484ec6db943516d6bd4f4
Shlomi Fish <shlomif@shlomifish.org>
2025-04-02 11:21:37 +0300

    pysolcards.py-for-C progress; rm multiply.py unused defs

--- multiply.py
+++ multiply.py
@@ -5,14 +5,6 @@ from pysol_cards.deal_game import Game
 from pysol_cards.random import RandomBase
 
 
-def pythonish_multiply(a, b):
-    print("Will compute", a, "times", b)
-    c = 0
-    for i in range(0, a):
-        c = c + b
-    return c
-
-
 rend = CardRenderer(True)
 
 
@@ -25,14 +17,3 @@ def create_gen(game_variant):
         s = game.calc_deal_string(deal_idx, rend)
         return s
     return _ret
-
-
-def gen_pysol_deal(game_variant, deal_idx):
-    ret = 0  # game.calc_deal_string(deal_idx, rend)
-    return ret
-
-
-class Gen:
-    def __init__(self):
-        self.game_variant = 'black_hole'
-        # self.game = Game(self.game_variant, 1, self.which_deals, 13)
