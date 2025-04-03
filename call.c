18853c0e5fdd9df5059c4b26309ba6ab0832c981
Shlomi Fish <shlomif@shlomifish.org>
2025-04-03 12:02:15 +0300

    Refactoring / code cleanup.
    See:
    
    * https://en.wikipedia.org/wiki/Code_refactoring
    
    * https://www.refactoring.com/
    
    * https://www.joelonsoftware.com/2002/01/23/rub-a-dub-dub/
    
    Some small optimisations may have slipped in as well.

--- call.c
+++ call.c
@@ -43,55 +43,55 @@ int main(int argc, char *argv[])
     /* pFunc is a new reference */
     char board_string[BOARD_STRING_SIZE];
 
-    if (pFunc && PyCallable_Check(pFunc))
-    {
-        PyObject *const pArgs = PyTuple_New(1);
-        for (int i = 0; i < 1; ++i)
-        {
-            PyObject *const create_gen_param =
-                ((i == 0) ? (PyUnicode_FromString(argv[1])) : NULL);
-            if (!create_gen_param)
-            {
-                Py_DECREF(pArgs);
-                Py_DECREF(global_python->pModule);
-                fprintf(stderr, "Cannot convert argument\n");
-                return 1;
-            }
-            /* create_gen_param reference stolen here: */
-            PyTuple_SetItem(pArgs, i, create_gen_param);
-        }
-        PyObject *const pFunc_gen = PyObject_CallObject(pFunc, pArgs);
-        Py_DECREF(pArgs);
-        if (!pFunc_gen)
-        {
-            Py_DECREF(pFunc);
-            Py_DECREF(global_python->pModule);
-            PyErr_Print();
-            fprintf(stderr, "Call failed\n");
-            return 1;
-        }
-        for (int argvidx = 2; argvidx < argc; ++argvidx)
-        {
-            const long deal_idx = atoi(argv[argvidx]);
-            const int ret_code =
-                pysol_cards__deal(board_string, pFunc_gen, deal_idx);
-            if (ret_code)
-            {
-                Py_DECREF(pArgs);
-                Py_DECREF(global_python->pModule);
-                fprintf(stderr, "Cannot convert argument\n");
-                return FAIL;
-            }
-            printf("%s", board_string);
-        }
-    }
-    else
+    if (!(pFunc && PyCallable_Check(pFunc)))
     {
         if (PyErr_Occurred())
             PyErr_Print();
         fprintf(stderr, "Cannot find game_variant \"%s\"\n", argv[2]);
+        goto cleanup_module;
+    }
+    PyObject *const pArgs = PyTuple_New(1);
+    for (int i = 0; i < 1; ++i)
+    {
+        PyObject *const create_gen_param =
+            ((i == 0) ? (PyUnicode_FromString(argv[1])) : NULL);
+        if (!create_gen_param)
+        {
+            Py_DECREF(pArgs);
+            Py_DECREF(global_python->pModule);
+            fprintf(stderr, "Cannot convert argument\n");
+            return 1;
+        }
+        /* create_gen_param reference stolen here: */
+        PyTuple_SetItem(pArgs, i, create_gen_param);
+    }
+    PyObject *const pFunc_gen = PyObject_CallObject(pFunc, pArgs);
+    Py_DECREF(pArgs);
+    if (!pFunc_gen)
+    {
+        Py_DECREF(pFunc);
+        Py_DECREF(global_python->pModule);
+        PyErr_Print();
+        fprintf(stderr, "Call failed\n");
+        return 1;
+    }
+    for (int argvidx = 2; argvidx < argc; ++argvidx)
+    {
+        const long deal_idx = atoi(argv[argvidx]);
+        const int ret_code =
+            pysol_cards__deal(board_string, pFunc_gen, deal_idx);
+        if (ret_code)
+        {
+            Py_DECREF(pArgs);
+            Py_DECREF(global_python->pModule);
+            fprintf(stderr, "Cannot convert argument\n");
+            return FAIL;
+        }
+        printf("%s", board_string);
     }
     Py_XDECREF(pFunc);
+
+cleanup_module:
     Py_DECREF(global_python->pModule);
 
     if (Py_FinalizeEx() < 0)
