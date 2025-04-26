69acf026ad6b72600fe000433ee6a28b55b7ad6b
Shlomi Fish <shlomif@shlomifish.org>
2025-04-26 08:15:24 +0300

    refactor: rearrange funcs order; add const

--- embed_python.h
+++ embed_python.h
@@ -26,52 +26,6 @@ typedef struct
     PyObject *create_gen;
 } pysol_cards__master_instance_type;
 
-static int pysol_cards__master_instance_init(
-    pysol_cards__master_instance_type *const master_instance,
-    global_python_instance_type *const global_python)
-{
-    PyObject *const create_gen =
-        PyObject_GetAttrString(global_python->py_module, "create_gen");
-    /* create_gen is a new reference */
-
-    if (!(create_gen && PyCallable_Check(create_gen)))
-    {
-        if (PyErr_Occurred())
-            PyErr_Print();
-        fprintf(stderr, "Cannot access create_gen() \"%s\"\n", "pysol_cards_c");
-        exit(PYSOL_CARDS__FAIL);
-    }
-    master_instance->create_gen = create_gen;
-    return PYSOL_CARDS__SUCCESS;
-}
-
-static int global_python_instance__release(
-    global_python_instance_type *const global_python)
-{
-    Py_DECREF(global_python->py_module);
-
-    if (Py_FinalizeEx() < 0)
-    {
-        return 120;
-    }
-}
-
-static void pysol_cards__master_instance_release(
-    pysol_cards__master_instance_type *const master_instance)
-{
-    Py_XDECREF(master_instance->create_gen);
-    master_instance->create_gen = NULL;
-}
-
-#define NUM_CARDS_DECKS 2
-#define NUM_CARDS_RANKS 13
-#define NUM_CARDS_SUITS 4
-#define NUM_CHARS_PER_CARD 3
-#define BOARD_STRING_MARGIN 512
-#define BOARD_STRING_SIZE                                                      \
-    (BOARD_STRING_MARGIN + NUM_CARDS_RANKS * NUM_CARDS_SUITS *                 \
-                               NUM_CARDS_DECKS * NUM_CHARS_PER_CARD)
-
 static int global_python_instance__init(
     global_python_instance_type *const global_python)
 {
@@ -91,12 +45,59 @@ static int global_python_instance__init(
     return PYSOL_CARDS__SUCCESS;
 }
 
+static int global_python_instance__release(
+    global_python_instance_type *const global_python)
+{
+    Py_DECREF(global_python->py_module);
+
+    if (Py_FinalizeEx() < 0)
+    {
+        return 120;
+    }
+}
+
+static int pysol_cards__master_instance_init(
+    pysol_cards__master_instance_type *const master_instance,
+    global_python_instance_type *const global_python)
+{
+    PyObject *const create_gen =
+        PyObject_GetAttrString(global_python->py_module, "create_gen");
+    /* create_gen is a new reference */
+
+    if (!(create_gen && PyCallable_Check(create_gen)))
+    {
+        if (PyErr_Occurred())
+            PyErr_Print();
+        fprintf(stderr, "Cannot access create_gen() \"%s\"\n", "pysol_cards_c");
+        exit(PYSOL_CARDS__FAIL);
+    }
+    master_instance->create_gen = create_gen;
+    return PYSOL_CARDS__SUCCESS;
+}
+
+static void pysol_cards__master_instance_release(
+    pysol_cards__master_instance_type *const master_instance)
+{
+    Py_XDECREF(master_instance->create_gen);
+    master_instance->create_gen = NULL;
+}
+
+#define NUM_CARDS_DECKS 2
+#define NUM_CARDS_RANKS 13
+#define NUM_CARDS_SUITS 4
+#define NUM_CHARS_PER_CARD 3
+#define BOARD_STRING_MARGIN 512
+#define BOARD_STRING_SIZE                                                      \
+    (BOARD_STRING_MARGIN + NUM_CARDS_RANKS * NUM_CARDS_SUITS *                 \
+                               NUM_CARDS_DECKS * NUM_CHARS_PER_CARD)
+
 typedef struct
 {
     PyObject *generator;
 } pysol_cards__generator_type;
 
-static void pysol_cards__create_generator(pysol_cards__generator_type *out,
+static void pysol_cards__create_generator(
+    pysol_cards__generator_type *const out,
     global_python_instance_type *const global_python, PyObject *const func,
     const char *const game_variant, int msdeals)
 {
