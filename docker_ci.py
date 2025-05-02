da4f85e5b94da89c870b3d58b2eec9784499dd14
Shlomi Fish <shlomif@shlomifish.org>
2025-05-02 11:34:25 +0300

    revise docker-based-CI
    based on https://pypi.org/project/podman/ ; thanks

---
+++ docker_ci.py
@@ -0,0 +1,48 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2025 Shlomi Fish < https://www.shlomifish.org/ >
+#
+# Licensed under the terms of the MIT license.
+
+"""
+
+"""
+
+import json
+from podman import PodmanClient
+
+"""Demonstrate PodmanClient."""
+
+# Provide a URI path for the libpod service.  In libpod, the URI can be a unix
+# domain socket(UDS) or TCP.  The TCP connection has not been implemented in
+# this package yet.
+
+uri = "unix:///run/user/1000/podman/podman.sock"
+
+with PodmanClient(base_url=uri) as client:
+    version = client.version()
+    print("Release: ", version["Version"])
+    print("Compatible API: ", version["ApiVersion"])
+    print("Podman API: ",
+          version["Components"][0]["Details"]["APIVersion"], "\n")
+
+    # get all images
+    for image in client.images.list():
+        print(image, image.id, "\n")
+
+    # find all containers
+    for container in client.containers.list():
+        # After a list call you would probably want to reload the container
+        # to get the information about the variables such as status.
+        # Note that list() ignores the sparse option and assumes True
+        # by default.
+        container.reload()
+        print(container, container.id, "\n")
+        print(container, container.status, "\n")
+
+        # available fields
+        print(sorted(container.attrs.keys()))
+
+    print(json.dumps(client.df(), indent=4))
